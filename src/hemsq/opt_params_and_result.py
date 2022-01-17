a3979912bea69b3666f05733598458b457b22dfc
CanonMukai <canon1white.rainbow@gmail.com>
2022-01-17 19:10:45 +0900

    Define Data class and make_result()

--- src/hemsq/opt_params_and_result.py
+++ src/hemsq/opt_params_and_result.py
@@ -1,3 +1,78 @@
+class Data:
+    def __init__(self,
+            name,
+            data,
+            tani,
+            color,
+        ):
+        self._name = name
+        self._data = data
+        self._tani = tani
+        self._color = color
+        self._name_with_tani = '{} ({})'.format(name, tani)
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def tani(self):
+        return self._tani
+
+    @property
+    def color(self):
+        return self._color
+
+    @property
+    def name_with_tani(self):
+        return self._name_with_tani
+
+
+def make_result(
+        sp,
+        demand_list,
+        sun_gen_list,
+        cost_ele_list,
+        sun_sell_price_list,
+        sun_use_list,
+        sun_charge_list,
+        sun_sell_list,
+        bat_out_list,
+        ele_use_list,
+        ele_charge_list,
+        bat_list,
+    ):
+    return {
+        'sp': sp,
+        'demand':
+            Data('Demand', demand_list, 'W', 'gray'),
+        'sun_gen':
+            Data('Solar Power Generation', sun_gen_list, 'W', 'black'),
+        'cost_ele':
+            Data('Commercial Electricity Prices', cost_ele_list, 'yen', 'red'),
+        'sun_sell_price':
+            Data('Solar-Sell Prices', sun_sell_price_list, 'yen', 'green'),
+        'sun_use':
+            Data('Use of Solar Power', sun_use_list, 'W', 'orangered'),
+        'sun_charge':
+            Data('Charge of Solar Power', sun_charge_list, 'W', 'orange'),
+        'sun_sell':
+            Data('Sales of Solar Power', sun_sell_list, 'W', 'blue'),
+        'bat_out':
+            Data('Use of Battery Electricity', bat_out_list, 'W', 'deepskyblue'),
+        'ele_use':
+            Data('Use of Commercial Electricity', ele_use_list, 'W', 'limegreen'),
+        'ele_charge':
+            Data('Charge of Commercial Electricity', ele_charge_list, 'W', 'yellow'),
+        'bat':
+            Data('Remaining amount of Battery', bat_list, 'W', 'pink'),
+    }
+
+
 class OptParamsAndResult:
     def __init__(self,
             sp=None,
